4d43be2dcf2a8e7e2dca3707b2ab5bf8cc956cb7
AliceLin <ke590pjmj7892@gmail.com>
2018-04-13 18:14:19 +0800

    update

--- all.js
+++ all.js
@@ -1,18 +1,39 @@
-var btn = document.querySelector('.add');
-btn.addEventListener('click',add,false);
-function add(e) {
-    var todo = document.querySelector('.todo');
-    var str = '';
-    // var text = '<li>'+ todo.value +'</li>';
-    console.log(todo.value);
-    // console.log(text);
-    var list = document.querySelector('.list');
-    var text = document.createElement('li');
-    text.setAttribute('class','line')
-    console.log(text);
-    text.textContent = todo.value ;
-    list.appendChild(text);
-    // var list = document.querySelector('.list');
-    // str+=text;
-    // list.innerHTML = text;
-}
+ // 指定 dom
+ var list = document.querySelector('.list');
+ var sendData = document.querySelector('.send');
+ var data = JSON.parse(localStorage.getItem('listData')) || [];
+
+ // 監聽與更新
+ sendData.addEventListener('click', addData);
+ list.addEventListener('click', toggleDone);
+ updateList(data);
+
+ //加入列表，並同步更新網頁與 localstorage
+ function addData(e) {
+   e.preventDefault();
+   var txt = document.querySelector('.text').value;
+   var todo = {
+     content: txt
+   };
+   data.push(todo);
+   updateList(data);
+   localStorage.setItem('listData', JSON.stringify(data));
+ }
+ // 更新網頁內容
+ function updateList(items) {
+   str = '';
+   var len = items.length;
+   for (var i = 0; len > i; i++) {
+     str += '<li><a href="#" data-index=' + i + ' />X</a> <span>' + items[i].content + '</span></li>';
+   }
+   list.innerHTML = str;
+ }
+ // 刪除代辦事項
+ function toggleDone(e) {
+   e.preventDefault();
+   if(e.target.nodeName !== 'A'){return};
+   var index = e.target.dataset.index;
+   data.splice(index, 1);
+   localStorage.setItem('listData', JSON.stringify(data));
+   updateList(data);
+ }
